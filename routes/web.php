eb8a1a00729c9dde829f3f1b68386e729e4ce3d8
alexis39tt <alessandro.fonzi@gmail.com>
2022-06-17 15:04:06 +0200

    added routes and auth

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('admin');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
 });
